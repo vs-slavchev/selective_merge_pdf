b24b4dbcaf1ffb8dc0924dafec56f94e452c1ebd
vs-slavchev <vs_slavchev2@abv.bg>
2019-10-26 22:53:56 +0200

    Add tests for exceptions

--- tests.py
+++ tests.py
@@ -15,5 +15,17 @@ class TestPageNumbersExtraction(unittest.TestCase):
 	def test_range_dash(self):
 		self.assertEqual(pages_args_to_array("5-8"), [5,6,7,8])
 
+	def test_wrong_input_word(self):
+		with self.assertRaises(Exception):
+			pages_args_to_array("word")
+
+	def test_wrong_input_char(self):
+		with self.assertRaises(Exception):
+			pages_args_to_array("1,c")
+
+	def test_wrong_input_char_range(self):
+		with self.assertRaises(Exception):
+			pages_args_to_array("e-5")
+
 if __name__ == '__main__':
     unittest.main()
